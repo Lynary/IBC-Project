587f56de17d896a2c2bf4919252cde859bad3ef8
unknown <krazefollow@gmail.com>
2021-10-06 22:44:07 +0700

    fadhel/nambahin redirect logout

--- layouts/Header.js
+++ layouts/Header.js
@@ -6,7 +6,7 @@ import { LogoutUser } from "../redux/store/actions/UserActions";
 import Image from 'next/image'
 export default function Header() {
   const { Header } = Layout;
-  const { pathname } = useRouter();
+  const { pathname,push } = useRouter();
   const isLogged = useSelector((state) => state.UserReducer.auth.success);
   const dispatch = useDispatch()
   const menu = [
@@ -27,6 +27,7 @@ export default function Header() {
   ];
   const Logout = () => {
     dispatch(LogoutUser())
+    push('/Login')
   }
   return (
     <>
